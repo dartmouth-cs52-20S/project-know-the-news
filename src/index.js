49191da716f3cb994c8546f9e15f047799f2ced0
Ayan Agarwal <ayan.agarwal0@gmail.com>
2020-04-30 23:42:54 -0500

    Committing Redux Additions

--- src/index.js
+++ src/index.js
@@ -5,13 +5,32 @@ import
 {
   BrowserRouter as Router, Route, NavLink, Switch,
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducers from './reducers';
+import Counter from './components/counter';
+import Controls from './components/controls';
+
+// import App from './components/app';
+
+// this creates the store with the reducers, and does some other stuff to initialize devtools
+// boilerplate to copy, don't have to know
+const store = createStore(reducers, {}, compose(
+  applyMiddleware(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+));
 
 const About = (props) => {
   return <div> All there is to know about me </div>;
 };
 
 const Welcome = (props) => {
-  return <div>Welcome</div>;
+  return (
+    <div>
+      <Counter />
+      <Controls />
+    </div>
+  );
 };
 
 const Test = (props) => {
@@ -51,4 +70,10 @@ const Nav = (props) => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('main'));
+// we now wrap App in a Provider
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('main'),
+);
