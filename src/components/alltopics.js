74b6950dc889a20817422c5a91044e7f9ec182b5
fmyagkov <fedor.m.myagkov.21@dartmouth.edu>
2020-06-08 18:38:28 +0300

    added search

--- src/components/alltopics.js
+++ src/components/alltopics.js
@@ -1,3 +1,5 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable consistent-return */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
@@ -6,39 +8,108 @@ import TextField from '@material-ui/core/TextField';
 import { fetchTopics } from '../actions/index';
 
 class AllTopics extends Component {
-/*   constructor(props) {
+  constructor(props) {
     super(props);
 
     this.state = {
-      filteredTopics: [],
+      searchValue: '',
+      // filteredTopics: [],
     };
 
-    this.filteredTopics = this.filteredTopics.bind(this);
-  } */
+    // this.filteredTopics = this.filteredTopics.bind(this);
+  }
 
   componentDidMount() {
     this.props.fetchTopics();
   }
 
+  searchValueChange = (event) => {
+    this.setState({ searchValue: event.target.value });
+  }
+
   topicsList = () => {
-    // if (document.getElementById('outlined-search') === '') {
+    if (this.state.searchValue === '') {
     // this.filteredTopics = this.props.topics.map((topic) => {
-    const topics = this.props.topics.map((topic) => {
-      return (
-        <li key={topic.id} className="postItem">
-          <NavLink to={`topics/${topic.id}`} exact id="link">
-            <Typography id="postTitle" variant="h4" component="h2">
-              {topic.title}
-            </Typography>
-            <Typography id="postTag" variant="subtitle1" component="h2" gutterBottom>
-              {`Created by: ${topic.authorUsername}`}
-            </Typography>
-          </NavLink>
-        </li>
-      );
-    });
-    return topics;
-    /*    }  else {
+      const topics = this.props.topics.map((topic) => {
+        return (
+          <li key={topic.id} className="postItem">
+            <NavLink to={`topics/${topic.id}`} exact id="link">
+              <Typography id="postTitle" variant="h4" component="h2">
+                {topic.title}
+              </Typography>
+              <Typography id="postTag" variant="subtitle1" component="h2" gutterBottom>
+                {`Created by: ${topic.authorUsername}`}
+              </Typography>
+            </NavLink>
+          </li>
+        );
+      });
+      return topics;
+    } else {
+      const topics = this.props.topics.map((topic) => {
+        const titleArray = topic.title.split(' ');
+        const searchArray = this.state.searchValue.split(' ');
+        for (let i = 0; i < searchArray.length; i += 1) {
+          for (let j = 0; j < titleArray.length; j += 1) {
+            if (searchArray[i].toLowerCase() === titleArray[j].toLowerCase()) {
+              return (
+                <li key={topic.id} className="postItem">
+                  <NavLink to={`topics/${topic.id}`} exact id="link">
+                    <Typography id="postTitle" variant="h4" component="h2">
+                      {topic.title}
+                    </Typography>
+                    <Typography id="postTag" variant="subtitle1" component="h2" gutterBottom>
+                      {`Created by: ${topic.authorUsername}`}
+                    </Typography>
+                  </NavLink>
+                </li>
+              );
+            }
+          }
+        }
+      });
+      return topics;
+    }
+  }
+
+  render() {
+    return (
+      <div id="topics-parent">
+        <div id="projName">
+          Topics
+        </div>
+        <div id="postsBG">
+          <div id="postsBGHead">
+            <div id="recent-topics">
+              All Topics
+            </div>
+            <TextField id="outlined-search"
+              label="Search Topics"
+              type="search"
+              variant="outlined"
+              value={this.state.searchValue}
+              onChange={this.searchValueChange}
+            />
+          </div>
+          <ul id="posts">
+            {/* {this.filteredTopics} */}
+            {this.topicsList()}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(reduxState) {
+  return {
+    topics: reduxState.topics.all,
+  };
+}
+
+export default connect(mapStateToProps, { fetchTopics })(AllTopics);
+
+/*    }  else {
       this.filteredTopics = this.props.topics.map((topic) => {
         console.log(topic.links[0]);
         return (
@@ -55,36 +126,3 @@ class AllTopics extends Component {
         );
       });
     } */
-  }
-
-  render() {
-    return (
-      <div id="topics-parent">
-        <div id="projName">
-          Topics
-        </div>
-        <div id="postsBG">
-          <div id="postsBGHead">
-            <div id="recent-topics">
-              All Topics
-            </div>
-            <TextField id="outlined-search" label="Search Topics" type="search" variant="outlined" />
-          </div>
-          <ul id="posts">
-            {/* {this.filteredTopics} */}
-            {this.topicsList()}
-          </ul>
-        </div>
-      </div>
-    );
-  }
-}
-
-function mapStateToProps(reduxState) {
-  console.log(reduxState);
-  return {
-    topics: reduxState.topics.all,
-  };
-}
-
-export default connect(mapStateToProps, { fetchTopics })(AllTopics);
