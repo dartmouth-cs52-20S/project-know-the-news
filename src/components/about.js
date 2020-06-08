7e79da710898343482820e14b9fbc26a93e560a0
Alex Feng <alex.feng.23@dartmouth.edu>
2020-06-08 07:42:26 -0700

    added to about page and fixed some styling

--- src/components/about.js
+++ src/components/about.js
@@ -22,12 +22,24 @@ class AboutPage extends Component {
           </div>
           <div className="about-section">
             <h2>Recommendation Methodology</h2>
-            <p>Filler text...</p>
+            <p>
+              Our Google Chrome extension provides recommended news sites by searching for related news articles
+              based on the current article title using Bing&apos;s News Search API. We then perform sentiment analysis
+              on the top search results using IBM Watson&apos;s Natural Processing API, sending back to the user a list of
+              news articles that are most similar and dissimilar in sentiment compared to that of the current article. In
+              the event that sentiment analysis cannot be performed on the current article--for instance, the API could
+              not extract the text on the website--the user will receive a list of articles with the most positive and
+              most negative sentiment. Unfortunately, we are not able to access articles behind subscription paywalls beyond
+              a preview text snippet on which sentiment analysis is performed.
+            </p>
           </div>
           <div className="about-section">
             <h2>Our Commitment to You</h2>
             <p>
-              We are comitted to...
+              We are comitted to ensuring that our users stay informed despite echo chambers that are ubiquitous across the
+              internet. What we do not do, however, is filter out &quot;fake news.&quot; Instead, we leave deciding what constitutes as
+              fake news up to the reader--our goal is to simply provide related articles for consideration along with a format
+              for discussion and organization.
             </p>
           </div>
         </div>
